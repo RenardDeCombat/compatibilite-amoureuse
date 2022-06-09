03142ce4e26f30243e25af1d1544aa30d7f684f7
RenardDeCombat <92103317+RenardDeCombat@users.noreply.github.com>
2022-06-09 19:26:40 +0200

    a

---
+++ script.js
@@ -0,0 +1,29 @@
+const premier = document.getElementById("premier")
+const deuxieme = document.getElementById("deuxieme")
+const button = document.getElementById("calculer-button")
+const resultat = document.getElementById("resultat")
+
+document.addEventListener("keyup", function(){
+    if(premier.value.length > 0 && deuxieme.value.length > 0) {
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+        resultat.classList.add("hidden");
+})
+
+button.addEventListener("click", function() {
+    const compatibilite = Math.floor(Math.random() * 100);
+
+    resultat.textContent = compatibilite + "%";
+
+    resultat.classList.remove("hidden");
+
+    button.disabled = true;
+});
+
+
+    
+
+
+
